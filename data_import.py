4eaecb23e3307faf6bf84d35f3ddd971a321ada6
WangZuoHao <85275362+wangzuohao@users.noreply.github.com>
2023-03-21 22:01:48 +0800

    Update data_import.py

--- data_import.py
+++ data_import.py
@@ -1,7 +1,15 @@
+# Import necessary packages
 from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader
 import os
-# Please setup the env for OpenAI API key
+
+# Please setup the env for OpenAI API key with the following command in Linux
 # export OPENAI_API_KEY='sk-xxxxxxxxx'
+
+# Loading from a directory ./src
 documents = SimpleDirectoryReader('./src').load_data()
+
+# Construct a simple vector index
 index = GPTSimpleVectorIndex(documents)
+
+# Save your index to a index.json file
 index.save_to_disk('index.json')
